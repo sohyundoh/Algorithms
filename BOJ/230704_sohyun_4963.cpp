a8bb1713c34c77b2bb8f2080f59771e7f4741a9e
sohyundoh <lyny.doh@gmail.com>
2023-07-05 15:48:31 +0900

    [BOJ] solve 4963

--- BOJ/230704_sohyun_4963.cpp
+++ BOJ/230704_sohyun_4963.cpp
@@ -1,33 +1,62 @@
 #include <iostream>
 #include <queue>
+#include <string.h>
 
 using namespace std;
-int dx[] = {0, 0, 1, -1, 1, 1, -1 , -1};
-int dy[] = {1,-1,0,0, - 1, 1 , 1, -1};
+int map[51][51];
+bool visit[51][51];
+int w, h;
+int dx[] = {0,0,-1,1,1,1,-1,-1};
+int dy[] = {1,-1,0,0,1,-1,-1,1};
+int cnt = 0;
+void bfs(int a, int b){
+    queue<pair<int, int>> q;
+    q.push(make_pair(a, b));
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 8;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-int solution(int w, int h){
-    int island = 0;
-    bool map[51][51] = {false, };
-    //입력받기
-    for(int i = 1; i<= w; i++){
-        for(int j = 1; j<=h; j++){
-            int i;
-            cin >> i;
-            if( i == 0){
-                continue;
+            if(nx >= 0 && nx < w && ny >=0 && ny < h){
+                if(!visit[ny][nx] && map[ny][nx] == 1){
+                    q.push(make_pair(nx, ny));
+                    visit[ny][nx] = true;
+                }
             }
-            map[i][j] = true;
         }
     }
-    //그래프 탐색
-
 }
 int main(){
-    int w,h;
-    cin >> w >> h;
-    while(w != 0  && h!=0){
-        cout << solution(w, h);
+    while(1){
         cin >> w >> h;
+        if(w==0 && h==0){
+            break;
+        }
+        cnt = 0;
+        memset(visit, false, sizeof(visit));
+        queue<pair<int, int>> queue;
+        for (int i = 0; i < h; i++){
+            for (int j = 0; j < w;j++){
+                cin >> map[i][j];
+                if(map[i][j]==1){
+                    queue.push(make_pair(j, i));
+                }
+            }
+        }
+        while(!queue.empty()){
+            int xx = queue.front().first;
+            int yy = queue.front().second;
+            if(!visit[yy][xx]){
+                visit[yy][xx] = true;
+                bfs(xx, yy);
+                cnt++;
+            }
+            queue.pop();
+        }
+        cout << cnt << '\n';
     }
     return 0;
 }
