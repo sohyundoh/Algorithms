da974b0021b6c7c0fbc399e74de491f5a5e86078
sohyundoh <lyny.doh@gmail.com>
2023-08-16 16:34:42 +0900

    solve 13458

---
+++ BOJ/230816_sohyun_13458.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N;
+	cin >> N;
+	
+	vector<int> V(N);
+	
+	for(int i=0; i<N; i++){
+		cin >> V[i];
+	}
+	
+	int B,C;
+	cin >> B >> C;
+	 
+	long long sum = 0;
+	
+	for(int i=0; i<N; i++){
+		if(V[i] <= B){
+			sum ++; 
+		}
+		else{
+			sum += (V[i]-B)/C + 1;
+			if((V[i]-B)%C != 0)
+				sum++;
+		}	
+	}
+	cout << sum;
+	return 0; 
+}
