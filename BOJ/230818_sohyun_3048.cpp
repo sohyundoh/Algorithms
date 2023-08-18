28cadb753f2fba442fd5c6d365f1b497b484f0ec
sohyundoh <lyny.doh@gmail.com>
2023-08-18 16:51:52 +0900

    solve 3048

--- BOJ/230818_sohyun_3048.cpp
+++ BOJ/230818_sohyun_3048.cpp
@@ -1,47 +1,53 @@
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int num1;
-    static int num2;
-    static List<Character> forward = new ArrayList<>();
-    static List<Character> backward = new ArrayList<>();
-    static List<Character> ants = new ArrayList<>();
-    static int maxLength;
-    public static String listToStringConverter() {
-        String s = "";
-        for (int i = 0; i < ants.size(); i++) {
-            s.concat(String.valueOf(ants.get(i)));
-        }
-        return s;
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N1 = Integer.parseInt(st.nextToken());
+		int N2 = Integer.parseInt(st.nextToken());
+		
+		String t1=br.readLine();
+		char[] tmp1 = new char[N1];
+		for (int i = 0; i < N1; i++) {
+			tmp1[i]=t1.charAt(N1-i-1);
+		}
+		
+		char[] tmp2 = br.readLine().toCharArray();
+		
+		char ants[]= new char[N1+N2];
+		int dir[] = new int[N1+N2];
+		for(int i = 0 ; i < N1;i++) {
+			ants[i]=tmp1[i];
+			dir[i]=1;
+		}
+		for (int i = N1; i < N1+N2; i++) {
+			ants[i]=tmp2[i-N1];
+			dir[i]=2;
+		}
+		int T = Integer.parseInt(br.readLine());
+		while(T>0) {
+			T--;
+			for (int i = 0; i < N1+N2-1; i++) {
+				if(dir[i]==1&&dir[i+1]==2) {
+					char t = ants[i];
+					ants[i]=ants[i+1];
+					ants[i+1]=t;
+					int td = dir[i];
+					dir[i]=dir[i+1];
+					dir[i+1]=td;
+					i++;
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N1+N2; i++) {
+			sb.append(ants[i]);
+		}
+		System.out.println(sb);
+	}
 
-    public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-        num1 = s.nextInt();
-        num2 = s.nextInt();
-        maxLength = Math.max(num1, num2);
-        String forwardInput = s.nextLine();
-        String backwardInput = s.nextLine();
-        for (int i = 0; i < num1; i++) {
-            forward.add(forwardInput.charAt(i));
-        }
-        for (int i = 0; i < num2; i++) {
-            backward.add(backwardInput.charAt(i));
-        }
-        ants.addAll(forward);
-        ants.addAll(backward);
-        int T = s.nextInt();
-
-        jump(T);
-        System.out.println(listToStringConverter());
-    }
-
-    public static void jump(int sec) {
-        if(sec<=0) return;
-
-    }
 }
