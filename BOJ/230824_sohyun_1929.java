49bf2a2aadc032403ecde21e758a8b8738c81c33
lynydoh <lyny.doh@gmail.com>
2023-08-26 16:29:15 +0900

    solve 1929

--- BOJ/230824_sohyun_1929.java
+++ BOJ/230824_sohyun_1929.java
@@ -1,22 +1,37 @@
-import java.io.IOException;
 import java.util.Scanner;
-
+ 
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-        int M = s.nextInt();
-        int N = s.nextInt();
-        for (int i = M; i <= N; i++) {
-            boolean flag = true;
-            for (int j = 2; j <= i - 1; j++) {
-                if (i % j == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                System.out.println(i);
-            }
-        }
-    }
+	public static boolean[] prime;
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		prime = new boolean[N + 1];
+		get_prime();
+				
+		StringBuilder sb = new StringBuilder();
+		for(int i = M; i <= N; i++) {
+			// false = 소수 
+			if(!prime[i]) sb.append(i).append('\n');
+		}
+		System.out.println(sb);
+	}
+ 
+ 
+	// 에라토스테네스의 체 알고리즘
+	public static void get_prime() {
+		// true = 소수아님 , false = 소수 
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
 }
+//에라토스테네스의 체
