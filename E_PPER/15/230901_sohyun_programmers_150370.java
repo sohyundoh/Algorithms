d029ec0e311f3c15a870bdc545a5a8de81b3868f
lynydoh <lyny.doh@gmail.com>
2023-09-01 16:53:08 +0900

    Update 230901_sohyun_programmers_150370.java

--- E_PPER/15/230901_sohyun_programmers_150370.java
+++ E_PPER/15/230901_sohyun_programmers_150370.java
@@ -1,6 +1,15 @@
 class Solution {
+    HashMap<String, String> term;
     public int[] solution(String today, String[] terms, String[] privacies) {
+        boolean 
         int[] answer = {};
         return answer;
+    }
+    private void termConverter(String[] terms){
+        term = new HashMap<>();
+        for(int i = 0; i< terms.length; i++){
+            String[] keyAndValue = terms[i].split(" ");
+            term.put(keyAndValue[0], keyAndValue[1]);
+        }
     }
 }
